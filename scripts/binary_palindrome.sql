20e449ba33e6720c7cdfd3d1d29b5e28eec5add3
Sebastian Pintos <47344141+SebastianPintos@users.noreply.github.com>
2023-06-19 15:09:14 -0300

    binary palindrome

---
+++ scripts/binary_palindrome.sql
@@ -0,0 +1,44 @@
+DELETE FROM programa;
+DELETE FROM traza_ejecucion;
+DELETE FROM alfabeto;
+
+SELECT * FROM programa;
+SELECT * FROM traza_ejecucion;
+SELECT * FROM alfabeto;
+
+INSERT INTO alfabeto VALUES ('0');
+INSERT INTO alfabeto VALUES ('1');
+INSERT INTO alfabeto VALUES ('_');
+
+INSERT INTO programa VALUES ('q0','0','qRight0','_','R');
+INSERT INTO programa VALUES ('qRight0','0','qRight0','0','R');
+INSERT INTO programa VALUES ('qRight0','1','qRight0','1','R');
+INSERT INTO programa VALUES ('q0','1','qRight1','_','R');
+INSERT INTO programa VALUES ('qRight1','0','qRight1','0','R');
+INSERT INTO programa VALUES ('qRight1','1','qRight1','1','R');
+INSERT INTO programa VALUES ('qRight0','_','qSearch0L','_','L');
+INSERT INTO programa VALUES ('qSearch0L','0','q1','_','L');
+INSERT INTO programa VALUES ('qRight1','_','qSearch1L','_','L');
+INSERT INTO programa VALUES ('qSearch1L','1','q1','_','L');
+INSERT INTO programa VALUES ('q1','0','qLeft0','_','L');
+INSERT INTO programa VALUES ('qLeft0','0','qLeft0','0','L');
+INSERT INTO programa VALUES ('qLeft0','1','qLeft0','1','L');
+INSERT INTO programa VALUES ('q1','1','qLeft1','_','L');
+INSERT INTO programa VALUES ('qLeft1','0','qLeft1','0','L');
+INSERT INTO programa VALUES ('qLeft1','1','qLeft1','1','L');
+INSERT INTO programa VALUES ('qLeft0','_','qSearch0R','_','R');
+INSERT INTO programa VALUES ('qSearch0R','0','q0','_','R');
+INSERT INTO programa VALUES ('qLeft1','_','qSearch1R','_','R');
+INSERT INTO programa VALUES ('qSearch1R','1','q0','_','R');
+INSERT INTO programa VALUES ('qSearch0R','1','qReject','1','-');
+INSERT INTO programa VALUES ('qSearch1R','0','qReject','0','-');
+INSERT INTO programa VALUES ('qSearch0L','1','qReject','1','-');
+INSERT INTO programa VALUES ('qSearch1L','0','qReject','0','-');
+INSERT INTO programa VALUES ('q0','_','f','_','_');
+INSERT INTO programa VALUES ('q1','_','f','_','_');
+INSERT INTO programa VALUES ('qSearch0L','_','f','_','_');
+INSERT INTO programa VALUES ('qSearch0R','_','f','_','_');
+INSERT INTO programa VALUES ('qSearch1L','_','f','_','_');
+INSERT INTO programa VALUES ('qSearch1R','_','f','_','_');
+
+SELECT simuladorMT('1010101');
--- scripts/turing_machine.sql
+++ scripts/turing_machine.sql
@@ -19,8 +19,8 @@ CREATE TABLE traza_ejecucion(
     estado_ori varchar(50),
     caracter_ori varchar(50),
     estado_nue varchar(50),
-    caracter_nue varchar(50) ,
-    desplazamiento varchar(50) 
+    caracter_nue varchar(50),
+    desplazamiento varchar(50)
 );
 
 CREATE TABLE alfabeto(
@@ -32,38 +32,44 @@ CREATE OR REPLACE FUNCTION simuladorMT(cinta_input varchar(200))
     AS $$
 DECLARE
     prog programa%ROWTYPE;
-    cin cinta%ROWTYPE;
     t_est varchar(50)
     DEFAULT 'q0';
     cin_long integer;
     i integer := 1;
-    pos integer := 1;
-	caracter char(1);
+    counter integer := 1;
+    pos integer := 2;
+    caracter char(1);
 BEGIN
-	cinta_input = CONCAT(cinta_input, '_');
+    cinta_input = CONCAT(cinta_input, '_');
+    cinta_input = CONCAT('_', cinta_input);
     cin_long := length(cinta_input);
-	
-	FOR i IN 1..LENGTH(cinta_input) LOOP
-		caracter = SUBSTRING(cinta_input FROM i FOR 1);
-		IF NOT EXISTS (SELECT * FROM alfabeto WHERE valor = caracter) THEN
-			RAISE NOTICE 'El caracter % no pertenece al lenguaje.', caracter;
-			RETURN;
-		END IF;
-	END LOOP;
-	
+    FOR i IN 1..LENGTH(cinta_input)
+    LOOP
+        caracter = SUBSTRING(cinta_input FROM i FOR 1);
+        IF NOT EXISTS (
+            SELECT
+                *
+            FROM
+                alfabeto
+            WHERE
+                valor = caracter) THEN
+        RAISE NOTICE 'El caracter % no pertenece al lenguaje.', caracter;
+        RETURN;
+    END IF;
+END LOOP;
     SELECT
         * INTO prog
     FROM
         programa
     WHERE
         estado_ori = 'q0'
-        AND caracter_ori = substring(cinta_input FROM pos FOR 1) LIMIT 1;
-	RAISE NOTICE 'programa : %', prog;
-	
-    WHILE t_est <> 'f' AND pos < length(cinta_input) LOOP
+        AND caracter_ori = substring(cinta_input FROM pos FOR 1)
+    LIMIT 1;
+    WHILE t_est <> 'f'
+    AND counter < 200 LOOP
         INSERT INTO traza_ejecucion(estado_ori, caracter_ori, estado_nue, caracter_nue, desplazamiento)
             VALUES (prog.estado_ori, prog.caracter_ori, prog.estado_nue, prog.caracter_nue, prog.desplazamiento);
-			
+        cinta_input = CONCAT(SUBSTRING(cinta_input FROM 1 FOR pos - 1), prog.caracter_nue, SUBSTRING(cinta_input FROM pos + 1));
         IF prog.estado_ori <> prog.estado_nue AND t_est <> prog.estado_nue THEN
             t_est := prog.estado_nue;
         END IF;
@@ -73,7 +79,7 @@ BEGIN
         IF prog.desplazamiento = 'L' THEN
             pos = pos - 1;
         END IF;
-		
+        counter = counter + 1;
         SELECT
             * INTO prog
         FROM
@@ -82,14 +88,15 @@ BEGIN
             estado_ori = t_est
             AND caracter_ori = substring(cinta_input FROM pos FOR 1)
         LIMIT 1;
-
     END LOOP;
-	
-	IF prog.estado_nue = 'f' THEN
-		INSERT INTO traza_ejecucion(estado_ori, caracter_ori, estado_nue, caracter_nue, desplazamiento)
+    RAISE NOTICE 'estado final %', prog.estado_nue;
+    IF t_est = 'f' THEN
+        INSERT INTO traza_ejecucion(estado_ori, caracter_ori, estado_nue, caracter_nue, desplazamiento)
             VALUES (prog.estado_ori, prog.caracter_ori, prog.estado_nue, prog.caracter_nue, prog.desplazamiento);
-	END IF;
-	
+        RAISE NOTICE 'El string SI pertenece al lenguaje';
+    ELSE
+        RAISE NOTICE 'El string NO pertenece al lenguaje';
+    END IF;
 END;
 $$
 LANGUAGE 'plpgsql';
