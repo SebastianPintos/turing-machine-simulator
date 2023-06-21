3732608e7c2dff77ee24e30f671e3b51bcb4bdf6
Juan M. Corbera <jcorbera@agrofy.com>
2023-06-21 00:38:00 -0300

    Se corrije lectura de extremos vacios

--- scripts/di_function.sql
+++ scripts/di_function.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION descripcionInstantanea()
     RETURNS VOID
     AS $$
@@ -14,7 +13,20 @@ BEGIN
         traza_ejecucion
     ORDER BY
         traza_id LOOP
-            result = result || substring(traza.cadena FROM 0 FOR i + 1) || '(' || traza.estado_ori || ')' || substring(traza.cadena FROM i + 1 FOR length(traza.cadena)) || ' |- ';
+			IF i = -1 THEN	
+				IF traza.estado_nue = 'f' THEN
+					result = result || substring(traza.cadena FROM 0 FOR i + 2) || '(' || traza.estado_ori || ')_' || substring(traza.cadena FROM i + 2 FOR length(traza.cadena));
+				ELSE
+					result = result || substring(traza.cadena FROM 0 FOR i + 2) || '(' || traza.estado_ori || ')_' || substring(traza.cadena FROM i + 2 FOR length(traza.cadena)) || ' |- ';
+				END IF;
+			ELSE
+				IF i < length(traza.cadena) THEN
+					result = result || substring(traza.cadena FROM 0 FOR i + 1) || '(' || traza.estado_ori || ')' || substring(traza.cadena FROM i + 1 FOR length(traza.cadena)) || ' |- ';
+				ELSE
+					result = result || substring(traza.cadena FROM 0 FOR i + 1) || '(' || traza.estado_ori || ')' || substring(traza.cadena FROM i + 1 FOR length(traza.cadena)) || '_ |- ';
+				END IF;
+			END IF;	
+            
             IF traza.desplazamiento = 'R' THEN
                 i = i + 1;
             END IF;
